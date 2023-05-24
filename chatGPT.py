b0fad5273a03be7681e9123a88ef2c55286ccf22
Ehsan Barkhordar <barkhordar.me@gmail.com>
2023-05-24 15:37:12 +0300

    use langchain

--- chatGPT.py
+++ chatGPT.py
@@ -16,8 +16,16 @@ chat_completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {
-            "role": "system",
-            "content": "Hello world"
+            "role": "user",
+            "content": """Erosion by the ocean:
+1. Wind creates waves in the ocean. 
+2. The waves wash onto the beaches. 
+3. The waves hit rocks on the beach. 
+4. Tiny parts of the rock break off.
+5. The rocks become smaller.
+
+I have the above procedural text. Create a python code for it which has different classes for the participants and their relationship with each other.
+"""
         }
     ]
 )
