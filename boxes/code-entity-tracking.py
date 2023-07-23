43a8007adcf0381bc48ba3172335ac8218cbfeeb
Ehsan Barkhordar <barkhordar.me@gmail.com>
2023-07-24 02:08:51 +0330

    code generation based on template works well

--- boxes/code-entity-tracking.py
+++ boxes/code-entity-tracking.py
@@ -1,6 +1,5 @@
 import os
 import json
-import re
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -15,7 +14,6 @@ ENGINE = "text-davinci-003"
 
 MAX_TOKENS = 4097
 TEMPERATURE = 0.3
-BOXES_CODE_PATH = '../boxes/code/{engine}/{ques_id}.py'
 
 openai.api_key = OPENAI_API_KEY
 
@@ -28,88 +26,35 @@ new_example_template = """{sentence}
 convert above text to a simple python code similar to previous one:"""
 
 
-def parse_output(output):
-    # Split the output into different box descriptions
-    box_descs = re.split(', Box ', output.strip())
-
-    box_dict = {}
-    for desc in box_descs:
-        # Extract the box number and the items
-        match = re.match(r'(\d+) contains (.*)', desc)
-
-        if match:
-            box_num = 'Box ' + match.group(1)
-            items = [item.strip() for item in re.split(', and | and |, ', match.group(2))]
-            box_dict[box_num] = items
-
-    return box_dict
-
-
-def calculate_metrics(final_states, result):
-    true_positives = 0
-    false_positives = 0
-    false_negatives = 0
-
-    for box, items in final_states.items():
-        result_items = result.get(box, [])
-
-        # remove trailing '.' if exists
-        result_items = [item.rstrip('.') for item in result_items]
-
-        # Convert lists to sets to make comparison easier
-        items_set = set(items)
-        result_set = set(result_items)
-
-        true_positives += len(items_set & result_set)
-        false_positives += len(result_set - items_set)
-        false_negatives += len(items_set - result_set)
-
-    return true_positives, false_positives, false_negatives
-
-
 def process_dataset():
-    aggregated_data_path = os.path.join(Settings.boxes_dataset, "aggregated_data.jsonl")
+    aggregated_data_path = os.path.join(Settings.boxes_dataset_path, "aggregated_data.jsonl")
     aggregated_boxes_file = open(aggregated_data_path, 'r')
     aggregated_boxes = list(aggregated_boxes_file)
 
-    code_example_path = os.path.join(Settings.boxes_dataset, "sample.py")
+    code_example_path = os.path.join(Settings.boxes_dataset_path, "sample.py")
     code_example_file = open(code_example_path, 'r')
+    example_code = code_example_file.read()
 
-    total_true_positives = 0
-    total_false_positives = 0
-    total_false_negatives = 0
-
-    for json_str in aggregated_boxes[31:37]:
+    for json_str in aggregated_boxes[20:25]:
         data = json.loads(json_str)
         sentence = data['sentence']
-        final_states = data['final_states']
+        sample_id = data['sample_id']
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "user", "content": example_0},
-                {"role": "assistant", "content": code_example_file.read()},
+                {"role": "assistant", "content": example_code},
                 {"role": "user", "content": new_example_template.format(sentence=sentence)},
             ],
             temperature=0,
         )
         output = response['choices'][0]['message']['content']
-        code_base_representation_path = Path(CODE_BASE_PATH.format(engine=MODEL_NAME, ques_id=ques_id))
+        code_representation_path = Path(Settings.boxes_code_path.format(engine=ENGINE, sample_id=sample_id))
 
-        true_positives, false_positives, false_negatives = calculate_metrics(final_states, result)
-        total_true_positives += true_positives
-        total_false_positives += false_positives
-        total_false_negatives += false_negatives
-
-    precision = total_true_positives / (total_true_positives + total_false_positives)
-    recall = total_true_positives / (total_true_positives + total_false_negatives)
-    f1_score = 2 * ((precision * recall) / (precision + recall))
-    accuracy = total_true_positives / (total_true_positives + total_false_positives + total_false_negatives)
-
-    print(f"Precision: {precision * 100:.2f}%")
-    print(f"Recall: {recall * 100:.2f}%")
-    print(f"F1 Score: {f1_score * 100:.2f}%")
-    print(f"Accuracy: {accuracy * 100:.2f}%")
+        with open(code_representation_path, 'w') as f:
+            f.write(output)
+        print(sample_id, "finished")
 
 
 process_dataset()
