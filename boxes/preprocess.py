6718c26fd087576e90635d85cdf988b477cb1ab4
Ehsan Barkhordar <barkhordar.me@gmail.com>
2023-07-25 01:35:05 +0330

    Fix preprocess and now it aggregate valid steps

--- boxes/preprocess.py
+++ boxes/preprocess.py
@@ -5,18 +5,21 @@ from collections import OrderedDict
 from settings import Settings
 
 dataset_path = os.path.join(Settings.boxes_dataset_path, "test-subsample-states-t5.jsonl")
-
 # Initialize an empty dictionary to store the aggregated data
 aggregated_data = {}
-
-# Open the JSON Lines file
+BOX_NUMBER = 7
 with open(dataset_path, 'r') as f:
-    # Read each line of the file
-    for line in f:
-        # Parse the JSON object from the line
-        item = json.loads(line)
+    lines = f.readlines()
+index = 0
+for i in range(0, len(lines), BOX_NUMBER):
+    group = lines[i:i + BOX_NUMBER]  # Get the next 7 lines (or fewer if not enough left)
+    items = [json.loads(line) for line in group]  # Parse the JSON objects from the lines
 
-        sample_id= item['sample_id']
+    final_states = {}
+    remaining_text = ""
+
+    for item in items:
+        sample_id = item['sample_id']
 
         # Formatted content to be appended
         sentence = item['sentence']
@@ -25,43 +28,27 @@ with open(dataset_path, 'r') as f:
         # To get the last sentence
         match = re.findall(pattern, sentence, flags=re.DOTALL)
         last_sentence = match[-1].strip() if match else 'No match'
-
-        # Split the final states into separate box states
-        box_states = last_sentence.split(', ')
-
-        # Initialize an empty OrderedDict to store the parsed box states
-        parsed_box_states = OrderedDict()
-
-        # Parse each box state
-        for box_state in box_states:
-            # Extract the box number and its contents
-            match = re.match(r'Box (\d) contains (.+)\.', box_state)
-            if match:
-                box_number = match.group(1)
-                box_contents = match.group(2).split(' and ')
-                # Convert the set into a list
-                box_contents = sorted(list(set(box_contents)))
-                # Add to the parsed box states
-                parsed_box_states['Box ' + box_number] = box_contents
-
-        # To get the remaining text
         remaining_text = re.sub(pattern, '', sentence, flags=re.DOTALL)
 
-        # If the sample_id is already in the aggregated data, update the formatted content
-        if item['sample_id'] in aggregated_data:
-            for box, items in parsed_box_states.items():
-                if box in aggregated_data[item['sample_id']]['final_states']:
-                    # Update the list, convert to set for union operation, and convert back to list for JSON compatibility
-                    aggregated_data[item['sample_id']]['final_states'][box] = sorted(list(set(aggregated_data[item['sample_id']]['final_states'][box]).union(set(items))))
-                else:
-                    aggregated_data[item['sample_id']]['final_states'][box] = sorted(items)
-        # If the sample_id is not in the aggregated data, add it with the formatted content
-        else:
-            del item['masked_content']
-            del item['sentence_masked']
-            aggregated_data[item['sample_id']] = item
-            aggregated_data[item['sample_id']]['sentence'] = remaining_text
-            aggregated_data[item['sample_id']]['final_states'] = parsed_box_states
+        # Initialize an empty OrderedDict to store the parsed box states
+        match = re.match(r'Box (\d) contains (.+)\.', last_sentence)
+        if match:
+            box_number = match.group(1)
+            box_contents = match.group(2).split(' and ')
+            # Convert the set into a list
+            box_contents = sorted(list(set(box_contents)))
+            # Add to the parsed box states
+            final_states['Box ' + box_number] = box_contents
+
+    # Add the parsed box states to the aggregated data
+    aggregated_data[index] = {
+        'sentence': remaining_text,
+        'sample_id': items[0]['sample_id'],
+        'numops': items[0]['numops'],
+        'final_states': final_states
+    }
+    index += 1
+
 
 dataset_path = os.path.join(Settings.boxes_dataset_path, "aggregated_data.jsonl")
 
@@ -69,3 +56,4 @@ with open(dataset_path, 'w') as outfile:
     for sample_id, data in aggregated_data.items():
         json.dump(data, outfile)
         outfile.write('\n')
+
