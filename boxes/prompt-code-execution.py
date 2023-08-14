5ff14cb13c3cb7691e080fbb7219cc05d44a1379
Ehsan Barkhordar <barkhordar.me@gmail.com>
2023-08-14 13:00:40 +0330

    do some clean code

--- boxes/prompt-code-execution.py
+++ boxes/prompt-code-execution.py
@@ -4,22 +4,22 @@ import openai
 import time
 
 ENGINE = "gpt-3.5-turbo"
-TEMPERATURE= 0
+TEMPERATURE = 0
 
 current_dir = pathlib.Path(__file__).parent.resolve()
 output_dir = os.path.join(current_dir, "code_output")
 boxes_code_path = os.path.join(current_dir, "code/{engine}".format(engine=ENGINE))
 
-def code_test():
+
+def ask_model_to_execute_the_code():
     example_code = open(os.path.join(current_dir, "sample1.py"), 'r')
     answer_output = open(os.path.join(current_dir, "sample1_answer.txt"), 'r')
     example = example_code.read()
     answer = answer_output.read()
 
-
     for code_hash in os.listdir(boxes_code_path):
         code_representation_path = pathlib.Path(os.path.join(boxes_code_path, "{filename}".format(filename=code_hash)))
-        if  code_representation_path.is_file():
+        if code_representation_path.is_file():
             sentence_hash = code_hash[:-3] + ".txt"
             output_path = pathlib.Path(os.path.join(output_dir, sentence_hash))
             if not output_path.is_file():
@@ -42,7 +42,8 @@ def code_test():
                         d.write(output)
                     print(code_hash, "finished")
                 except openai.error.RateLimitError:
+                    print("sleeping")
                     time.sleep(20)
 
 
-code_test()
+ask_model_to_execute_the_code()
