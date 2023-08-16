9d86368907e4fdb517a8072a7263613ee942e5ea
Mahammad Nahmadov <56833839+mnahmadov@users.noreply.github.com>
2023-08-16 14:55:12 +0200

    Added generate_examples.py v1

--- boxes/generate_examples.py
+++ boxes/generate_examples.py
@@ -1,31 +1,59 @@
+import hashlib
 import random
+import json
 
 # Extended List of Operations
 operations = ["Put", "Move", "Remove", "Empty", "Replace", "Swap"]
-content_items = ["apple", "book", "candle", "pen", "hat", "gloves", "shoes", "socks", "watch", "ring",
-                 "necklace", "spoon", "fork", "shirt", "pants", "flashlight", "mug", "scarf", "battery",
-                 "key", "glasses", "knife", "pillow", "camera", "tape", "charger", "remote",
-                 "map", "ball", "painting", "pliers", "hammer", "screwdriver", "bottle", "cork",
-                 "wallet", "perfume", "brush", "comb", "soap", "toothbrush", "toothpaste", "towel",
-                 "umbrella", "guitar", "microphone", "headphones", "calculator", "wallet", "coin", "phone",
-                 "tablet", "laptop", "keyboard", "mouse", "blanket", "lamp", "clock", "mirror",
-                 "shovel", "plant", "flower", "scissors", "plate", "bowl", "cup", "fork", "knife",
-                 "spoon", "glass", "candlestick", "vase", "frame", "portrait", "fan", "shampoo",
-                 "conditioner", "razor", "hairbrush", "bandage", "ointment", "socks", "underwear", "shirt",
-                 "pants", "dress", "tie", "jacket", "coat", "belt", "bracelet", "watch", "earrings",
-                 "necklace", "paper", "pencil", "pen", "marker", "crayon", "chalk", "ruler", "eraser",
-                 "notebook", "calendar", "envelope", "stamp", "stapler", "scissors", "tape", "glue",
-                 "folder", "backpack", "briefcase", "lunchbox", "book", "magazine", "newspaper", "dictionary",
-                 "thesaurus", "encyclopedia", "novel", "poetry", "biography", "history", "fiction", "nonfiction",
-                 "mystery", "romance", "science", "fantasy", "adventure", "horror", "comedy", "drama",
-                 "music", "art", "film", "photograph", "exhibition", "concert", "ticket",
-                 "recipe", "ingredient", "grocery", "shopping", "cart", "basket", "bag", "wallet", "purse",
-                 "cashier", "credit", "debit", "receipt", "change", "coin", "bank",
-                 "account", "loan", "interest", "investment", "stock", "portfolio", "retirement", "insurance",
-                 "claim", "deductible", "premium", "coverage", "policy", "badge", "uniform", "weapon", "shield"]
+content_items = ["the apple", "the book", "the candle", "the pen", "the hat", "the gloves", "the shoes", "the socks",
+                 "the watch", "the ring", "the necklace", "the spoon", "the fork", "the shirt", "the pants",
+                 "the flashlight", "the mug", "the scarf", "the battery", "the key", "the glasses", "the knife",
+                 "the pillow", "the camera", "the tape", "the charger", "the remote", "the map", "the ball",
+                 "the painting", "the pliers", "the hammer", "the screwdriver", "the bottle", "the cork", "the wallet",
+                 "the perfume", "the brush", "the comb", "the soap", "the toothbrush", "the toothpaste", "the towel",
+                 "the umbrella", "the guitar", "the microphone", "the headphones", "the calculator", "the wallet",
+                 "the coin", "the phone", "the tablet", "the laptop", "the keyboard", "the mouse", "the blanket",
+                 "the lamp", "the clock", "the mirror", "the shovel", "the plant", "the flower", "the scissors",
+                 "the plate", "the bowl", "the cup", "the fork", "the knife", "the spoon", "the glass",
+                 "the candlestick", "the vase", "the frame", "the portrait", "the fan", "the shampoo",
+                 "the conditioner", "the razor", "the hairbrush", "the bandage", "the ointment", "the socks",
+                 "the underwear", "the shirt", "the pants", "the dress", "the tie", "the jacket", "the coat",
+                 "the belt", "the bracelet", "the watch", "the earrings", "the necklace", "the paper", "the pencil",
+                 "the pen", "the marker", "the crayon", "the chalk", "the ruler", "the eraser", "the notebook",
+                 "the calendar", "the envelope", "the stamp", "the stapler", "the scissors", "the tape", "the glue",
+                 "the folder", "the backpack", "the briefcase", "the lunchbox", "the book", "the magazine",
+                 "the newspaper", "the dictionary", "the thesaurus", "the encyclopedia", "the novel", "the poetry",
+                 "the biography", "the history", "the fiction", "the nonfiction", "the mystery", "the romance",
+                 "the science", "the fantasy", "the adventure", "the horror", "the comedy", "the drama", "the music",
+                 "the art", "the film", "the photograph", "the exhibition", "the concert", "the ticket", "the recipe",
+                 "the ingredient", "the grocery", "the shopping", "the cart", "the basket", "the bag", "the wallet",
+                 "the purse", "the cashier", "the credit", "the debit", "the receipt", "the change", "the coin",
+                 "the bank", "the account", "the loan", "the interest", "the investment", "the stock", "the portfolio",
+                 "the retirement", "the insurance", "the claim", "the deductible", "the premium", "the coverage",
+                 "the policy", "the badge", "the uniform", "the weapon", "the shield"]
+
+file_path_aggregated_data = "../datasets/boxes-only-needed/aggregated_data.jsonl"
+sentence_hashes_used = set()  # set to store sentence_hash values
+
+
+def hash_sentence(text, length=10):
+    # To get sentence_hash values
+    full_hash = hashlib.sha256(text.encode()).hexdigest()
+    return full_hash[:length]
+
+
+with open(file_path_aggregated_data, "r") as file:
+    for line in file:
+        data = json.loads(line)  # get the JSON from each line
+        sentence_hash = data.get("sentence_hash")  # get the sentence hash of the current JSON
+        if sentence_hash:
+            sentence_hashes_used.add(sentence_hash)  # add sentence hash to the used hashes set
+
+
+# print(len(sentence_hashes_used))
 
 
 def generate_procedural_text(total_operations_count, total_boxes_count):
+    numops = {"Move": 0, "Remove": 0, "Put": 0, "Empty": 0, "Replace": 0, "Swap": 0, "Total": 0}
     # Randomly generating initial box contents
     box_contents = {}  # Keeping track of box contents for some operations to make sense, e.g. Swap, Replace
     for i in range(total_boxes_count):
@@ -60,15 +88,20 @@ def generate_procedural_text(total_operations_count, total_boxes_count):
         boxes_with_items = [box for box, items in box_contents.items() if items]
 
         operation = random.choice(operations)
+
+        # Keeping track of numops for our JSON
+        numops[str(operation)] += 1
+        numops["Total"] += 1
+
         # THINK OF APPLYING AN OPERATION TO MULTIPLE ITEMS AT THE SAME TIME
         # E.G. Put the dish and the stone into Box 5.
         if operation == "Put":
             destination = random.choice(list(box_contents.keys()))  # First find the destination
             item = random.choice(content_items)
-            while item in box_contents[destination]:    # if the item is already there, keep searching for a new item
+            while item in box_contents[destination]:  # if the item is already there, keep searching for a new item
                 item = random.choice(content_items)
             box_contents[destination].append(item)  # Put the item into the box
-            actions = f"{operation} the {item} into {destination}."
+            actions = f"{operation} {item} into {destination}."
 
         elif operation == "Move":
             if boxes_with_items:
@@ -79,14 +112,14 @@ def generate_procedural_text(total_operations_count, total_boxes_count):
                 # print(item)
                 box_contents[source].remove(item)
                 box_contents[destination].append(item)
-                actions = f"{operation} the {item} from {source} to {destination}."
+                actions = f"{operation} {item} from {source} to {destination}."
 
         elif operation == "Remove":
             if boxes_with_items:
                 box = random.choice(boxes_with_items)
                 item = random.choice(box_contents[box])
                 box_contents[box].remove(item)
-                actions = f"{operation} the {item} from {box}."
+                actions = f"{operation} {item} from {box}."
 
         elif operation == "Empty":
             if boxes_with_items:
@@ -103,14 +136,14 @@ def generate_procedural_text(total_operations_count, total_boxes_count):
                     new_item = random.choice(new_item_candidates)
                     box_contents[box].remove(item_to_replace)
                     box_contents[box].append(new_item)
-                    actions = f"{operation} the {item_to_replace} with the {new_item} in {box}."
+                    actions = f"{operation} {item_to_replace} with {new_item} in {box}."
 
         elif operation == "Swap":
             if len(boxes_with_items) >= 2:  # If there are at least 2 boxes with items, we can do swap
                 box1, box2 = random.sample(boxes_with_items, 2)
                 item1 = random.choice(box_contents[box1])
                 item2 = random.choice(box_contents[box2])
-                while item1 == item2:   # If both are items are the same, search for new ones
+                while item1 == item2:  # If both are items are the same, search for new ones
                     item1 = random.choice(box_contents[box1])
                     item2 = random.choice(box_contents[box2])
 
@@ -118,21 +151,49 @@ def generate_procedural_text(total_operations_count, total_boxes_count):
                 box_contents[box2].remove(item2)
                 box_contents[box1].append(item2)
                 box_contents[box2].append(item1)
-                actions = f"{operation} the {item1} in {box1} with the {item2} in {box2}."
+                actions = f"{operation} {item1} in {box1} with {item2} in {box2}."
 
         operations_text.append(actions)
 
     # Combining box descriptions and operations: Getting the procedural text
     procedural_text += " " + " ".join(operations_text)
+
+    sentence_hash_value = hash_sentence(procedural_text)
+    while sentence_hash_value in sentence_hashes_used:  # check if the current hash has previously been used
+        print("PREVIOUSLY USED HASH VALUE")
+        sentence_hash_value = hash_sentence(procedural_text)
+    sentence_hashes_used.add(sentence_hash_value)
+
+    final_json = {"sentence_hash": sentence_hash_value,
+                  "sentence": procedural_text,
+                  "sample_id": -1,
+                  "numops": numops,
+                  "final_states": box_contents}
     # print(procedural_text)
-    return procedural_text, box_contents
+    # print(box_contents)
+    return final_json
 
 
 # # Example usage
-num_operations = int(input("Enter the number of total operations: "))
-num_boxes = int(input("Enter the number of boxes: "))
+data_list = []
+for i in range(800):  # 500 examples
+    num_operations = random.randint(5, 20)
+    num_boxes = random.randint(5, 15)
+    example = generate_procedural_text(num_operations, num_boxes)
+    example["sample_id"] = i
+    data_list.append(example)
 
-procedural_text, final_states = generate_procedural_text(num_operations, num_boxes)
-print(procedural_text)
-print(final_states)
+# print(procedural_text)
+# print(final_states)
 
+# Creating new JSONL file
+jsonl_filepath = "new_aggregated_data.jsonl"
+
+# Write each JSON object as a line in the JSONL file
+with open(jsonl_filepath, 'w') as jsonl_file:
+    for data in data_list:
+        json_line = json.dumps(data, ensure_ascii=False)
+        jsonl_file.write(json_line + '\n')
+
+print(len(sentence_hashes_used))
+print("JSONL file created successfully.")
