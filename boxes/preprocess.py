b891b8155172a1ecd0f0e14ac5cd2ab97afb7590
Ehsan Barkhordar <barkhordar.me@gmail.com>
2023-09-03 21:44:02 +0300

    make preprocess.py better

--- boxes/preprocess.py
+++ boxes/preprocess.py
@@ -1,82 +1,77 @@
+import argparse
 import hashlib
 import json
-import os
 import re
-from settings import Settings
 
 
 def get_number_operations(text):
-    move_operations = len(re.findall(r"\bMove\b", text))
-    remove_operations = len(re.findall(r"\bRemove\b", text))
-    put_operations = len(re.findall(r"\bPut\b", text))
-
+    """Count the number of Move, Remove, and Put operations in the text."""
     operations = {
-        "Move": move_operations,
-        "Remove": remove_operations,
-        "Put": put_operations
+        "Move": len(re.findall(r"\bMove\b", text)),
+        "Remove": len(re.findall(r"\bRemove\b", text)),
+        "Put": len(re.findall(r"\bPut\b", text))
     }
-
-    total_operations = sum(operations.values())
-    operations["Total"] = total_operations
-
+    operations["Total"] = sum(operations.values())
     return operations
 
 
 def hash_sentence(text, length=10):
-    full_hash = hashlib.sha256(text.encode()).hexdigest()
-    return full_hash[:length]
+    """Hash a sentence and return a specified number of characters."""
+    return hashlib.sha256(text.encode()).hexdigest()[:length]
 
 
-dataset_path = os.path.join(Settings.boxes_dataset_path, "test-subsample-states-t5.jsonl")
-aggregated_data = {}
-BOX_NUMBER = 7
-INDEX = 0
+def process_dataset(input_path, output_path, box_number=7):
+    aggregated_data = {}
+    index = 0
 
-with open(dataset_path, 'r') as f:
-    lines = f.readlines()
+    with open(input_path, 'r') as f:
+        lines = f.readlines()
 
-for i in range(0, len(lines), BOX_NUMBER):
-    group = lines[i:i + BOX_NUMBER]  # Get the next 7 lines (or fewer if not enough left)
-    items = [json.loads(line) for line in group]  # Parse the JSON objects from the lines
+    for group_start_index in range(0, len(lines), box_number):
+        group = lines[group_start_index:group_start_index + box_number]
+        items = [json.loads(line) for line in group]
 
-    final_states = {}
-    remaining_text = ""
+        final_states = {}
+        remaining_text = ""
 
-    for item in items:
-        sample_id = item['sample_id']
+        for item in items:
+            sentence = item['sentence']
 
-        # Formatted content to be appended
-        sentence = item['sentence']
-        pattern = r'\s*[^.]*\.$'
+            # Extract the last sentence from the text.
+            match = re.findall(r'\s*[^.]*\.$', sentence, flags=re.DOTALL)
+            last_sentence = match[-1].strip() if match else 'No match'
+            remaining_text = re.sub(r'\s*[^.]*\.$', '', sentence, flags=re.DOTALL)
 
-        # To get the last sentence
-        match = re.findall(pattern, sentence, flags=re.DOTALL)
-        last_sentence = match[-1].strip() if match else 'No match'
-        remaining_text = re.sub(pattern, '', sentence, flags=re.DOTALL)
+            # Extract box number and contents.
+            match = re.match(r'Box (\d) contains (.+)\.', last_sentence)
+            if match:
+                matched_box_number = match.group(1)
+                box_contents = sorted(list(set(match.group(2).split(' and '))))
+                final_states['Box ' + matched_box_number] = box_contents
 
-        # Initialize an empty OrderedDict to store the parsed box states
-        match = re.match(r'Box (\d) contains (.+)\.', last_sentence)
-        if match:
-            box_number = match.group(1)
-            box_contents = match.group(2).split(' and ')
-            # Convert the set into a list
-            box_contents = sorted(list(set(box_contents)))
-            # Add to the parsed box states
-            final_states['Box ' + box_number] = box_contents
+        aggregated_data[index] = {
+            'sentence_hash': hash_sentence(remaining_text),
+            'sentence': remaining_text,
+            'sample_id': items[0]['sample_id'],
+            'numops': get_number_operations(remaining_text),
+            'final_states': final_states
+        }
+        index += 1
 
-    # Add the parsed box states to the aggregated data
-    aggregated_data[INDEX] = {
-        'sentence_hash': hash_sentence(remaining_text),
-        'sentence': remaining_text,
-        'sample_id': items[0]['sample_id'],
-        'numops': get_number_operations(remaining_text),
-        'final_states': final_states
-    }
-    INDEX += 1
+    with open(output_path, 'w') as outfile:
+        for data in aggregated_data.values():
+            json.dump(data, outfile)
+            outfile.write('\n')
 
-dataset_path = os.path.join(Settings.boxes_dataset_path, "aggregated_data.jsonl")
 
-with open(dataset_path, 'w') as outfile:
-    for sample_id, data in aggregated_data.items():
-        json.dump(data, outfile)
-        outfile.write('\n')
+if __name__ == '__main__':
+    # Setting up command line arguments
+    parser = argparse.ArgumentParser(description='Process dataset to aggregate data.')
+    parser.add_argument('input_dataset_path', type=str, help='Path to the input dataset.')
+    parser.add_argument('output_dataset_path', type=str, help='Path to where the aggregated data should be saved.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Use the parsed arguments
+    process_dataset(args.input_dataset_path, args.output_dataset_path)
